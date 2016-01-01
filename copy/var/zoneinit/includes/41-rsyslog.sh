11a38d480027ddc63d95e903966ffd35cf034ff1
Arthur van der Peijl <aavanderpeijl@gmail.com>
2016-01-01 14:28:58 +0100

    Logging enabled for real now

--- copy/var/zoneinit/includes/41-rsyslog.sh
+++ copy/var/zoneinit/includes/41-rsyslog.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Pacakge overrides our copied file
+mv /opt/local/etc/rsyslog_haproxy.conf /opt/local/etc/rsyslog.conf
+
 /usr/sbin/svcadm disable svc:/system/system-log:default
 /usr/sbin/svcadm enable svc:/pkgsrc/rsyslog:default
