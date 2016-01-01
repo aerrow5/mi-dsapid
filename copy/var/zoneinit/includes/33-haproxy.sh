4674739f51d4e05a32e767a4b915a65dbd302eeb
Arthur van der Peijl <aavanderpeijl@gmail.com>
2016-01-01 14:22:52 +0100

    Enable haproxy logging

--- copy/var/zoneinit/includes/33-haproxy.sh
+++ copy/var/zoneinit/includes/33-haproxy.sh
@@ -3,6 +3,7 @@
 
 test -d /data/ssl || mkdir /data/ssl
 
+mkdir -p /var/lib/haproxy/dev
 logadm -C 10 -p 1d -a 'kill -HUP `cat /var/run/*syslog*.pid`' -w haproxy /var/log/haproxy/*.log
 
 /usr/sbin/svcadm enable svc:/pkgsrc/haproxy:default
